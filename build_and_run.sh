da476cf865121c7ad2df5acfc611a5a277428c8b
tewelle <tewelle.welemariam@cag.se>
2022-11-26 16:13:26 -0600

    Added k8s deployment

---
+++ build_and_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $# -gt 0 ] && [ $1 = "-b" ]
+then
+  mvn clean package -DskipTests
+  docker build . -t cicd-demo
+  # tag and push image
+  docker tag cicd-demo tewemit/cicd-demo
+  docker push tewemit/cicd-demo
+else
+    echo Skipping build
+fi
+
+# deploy to kubernetes
+if [ $# -gt 1 ] && [ $2 = "-d" ]
+then
+kubectl delete svc cicd-service
+kubectl delete deployment cicd-service
+kubectl apply -f deployment.yaml
+else
+  echo Skipping deployment
+fi
